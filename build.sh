58310cdccd5fb87447bd621e488bdc3c30135100
Marco Kurzynski <marcokurzynski@icloud.com>
2025-12-23 15:12:27 -0500

    bind mount to results, and print when build and run complete

--- build.sh
+++ build.sh
@@ -7,8 +7,14 @@
 #SBATCH --gres=gpu:tesla_v100-pcie-32gb:1
 #SBATCH --output=sbatch_build_%j.out
 
-set -ux
+# allow errors for clearing /tmp
+set -u
 
 module load apptainer
+
+# clear /tmp to not run out of space
 rm -rf /tmp/*
+
+echo "SBATCH_BUILD_START $(date +%s)"
 apptainer build --nv -F dreamplace.sif dreamplace.def
+echo "SBATCH_BUILD_START $(date +%s)"
--- run.sh
+++ run.sh
@@ -7,8 +7,13 @@
 #SBATCH --gres=gpu:tesla_v100-pcie-32gb:1
 #SBATCH --output=sbatch_run_%j.out
 
-set -eux
+set -eu
 
 module load apptainer
 
-apptainer exec --cwd /opt/DREAMPlace --nv dreamplace.sif python install/dreamplace/Placer.py test/ispd2005/adaptec1.json
+cd $SLURM_SUBMIT_DIR
+[[ -d results ]] || mkdir results
+
+echo "SBATCH_RUN_START $(date +%s)"
+apptainer exec --bind results:/opt/DREAMPlace/results --cwd /opt/DREAMPlace --nv dreamplace.sif python install/dreamplace/Placer.py test/ispd2005/adaptec1.json
+echo "SBATCH_RUN_STOP $(date +%s)"
